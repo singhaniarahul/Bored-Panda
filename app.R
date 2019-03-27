d103b7e2fe0a486a0edff20e0df8ffc74e8be580
Rahul Singhania <rahulsinghania0820@gmail.com>
2019-03-27 16:01:22 +0530

    Comments Added

--- app.R
+++ app.R
@@ -1,20 +1,26 @@
 #
 # Developed by : Rahul Singhania
 # 
+#Load the packages
 library(shiny)
 library(dplyr)
 library(DT)
 library(shinythemes)
 library(emojifont)
 
+#Read the movie data from a csv file
 moviedata <- readxl::read_excel("Movie.xlsx")
 attach(moviedata)
+
+#Read the TV data from a csv file
 tvdata <- readxl::read_excel("TV.xlsx")
 attach(tvdata)
+
+#Extract Max Runtime for a movie, to be used later as an upper limit in input options
 max_rt <- max(moviedata$Runtime_Min)
 
 ui <- fluidPage(
-  
+  #Application Theme
   theme = shinytheme("spacelab"),
   # Application title
   titlePanel("Bored Panda"),
@@ -129,6 +135,7 @@ server <- function(input, output) {
   
   output$dispTable <- renderDataTable({
     x <- moviedata
+    #Choose between TV Show Data and Movie Data
     if(input$choice==1)
     {
       col = c(1,2,3,4,5,6,7,8,9,10)
@@ -139,38 +146,54 @@ server <- function(input, output) {
       col = c(1,2,3,4,5,6,7,9)
       x<- tvdata
     }
+    #If range of year of release is mentioned
     if(input$get_year)
     {
+      #If genre is mentioned
       if(input$get_genre)
       {
+        #Filter the data
         moviedata_genre <- dplyr::filter(x, grepl(input$genre, x$Genre))
+        #Subset the data
         moviedata_genre_year<- moviedata_genre[moviedata_genre$Year>=input$styear & moviedata_genre$Year<=input$enyear & moviedata_genre$Rating>=input$rating & moviedata_genre$Runtime_Min<=input$rt,col]
+        #Render
         DT::datatable(data = moviedata_genre_year, 
                       options = list(pageLength = 15), 
                       rownames = FALSE
                       ) 
       }
+      #If genre is not avaialble
       else
       {
+        #Subset the data
         moviedata_year<- x[x$Year>=input$styear & x$Year<=input$enyear & x$Rating>=input$rating & x$Runtime_Min<=input$rt,col]
+        #Render the data
         DT::datatable(data = moviedata_year, 
                       options = list(pageLength = 15), 
                       rownames = FALSE) 
       }
     }
+    #If range of release year is not mentioned
     else
     {
+      #If genre is mentioned
       if(input$get_genre)
       {
+        #Filter the data
         moviedata_genre <- dplyr::filter(x, grepl(input$genre, x$Genre))
+        #Subset the data
         moviedata_genre<- moviedata_genre[moviedata_genre$Rating>=input$rating & moviedata_genre$Runtime_Min<=input$rt,col]
+        #Render the data
         DT::datatable(data = moviedata_genre, 
                       options = list(pageLength = 15), 
                       rownames = FALSE) 
       }
+      #Genre not mentioned
       else
       {
+        #Subset the data
         moviedataOut<- x[x$Rating>=input$rating & x$Runtime_Min<=input$rt,col]
+        #Render the data
         DT::datatable(data = moviedataOut, 
                       options = list(pageLength = 15), 
                       rownames = FALSE) 
